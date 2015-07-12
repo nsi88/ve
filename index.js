181692a682ad0bd14e7007cf100558a350253da9
Sergey Novikov <novikovseregka@gmail.com>
2015-07-12 17:21:06 +0300

    Preprocessing

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ var $ = require('jquery');
 
 $(function(){
   var mustache = require('mustache');
-  
+  var files = [];
+  var preprocessor = new Worker('./lib/preprocessor.js');
+
+  preprocessor.onmessage = function(event) {
+    var m = event.data;
+    if (m.type == 'log') {
+      console.log(m.data);
+    } else if (m.type == 'image') {
+      console.log(m.data);
+    }
+  };
+
   $('#add').change(function() {
-    var files = $(this)[0].files;
-    for (var i = 0; i < files.length; i++) {
-      $('#files').append(mustache.render($('#file').html(), { file: files[i] }));
+    for (var i = 0; i < $(this)[0].files.length; i++) {
+      var file = $(this)[0].files[i];
+      $('#files').append(mustache.render($('#file').html(), { file: file }));
+      files.push(file);
+      preprocessor.postMessage(file);
     }
   });
 });
